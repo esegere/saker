1724e7800a683e323d13da99b8747ce989180616
Sergio <garciasergiomar@gmail.com>
2021-08-02 15:36:14 -0500

    added job watching

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ int main(int argc, const char* argv[]) {
     cli("l", "0") >> line_number;
     int prev_error_code;
     cli("e", "0") >> prev_error_code;
+    int jobs_managed;
+    cli("j", "0") >> jobs_managed;
     
     if (!git_branch.empty()) {
         const auto last_to_erase = std::find_if(
@@ -43,7 +45,7 @@ int main(int argc, const char* argv[]) {
     
     std::cout <<
               saker::Prompt{
-        
+    
                   saker::Zone{ // line number
                       std::to_string(line_number)
                   }.bg(saker::Bg::black)
@@ -52,26 +54,39 @@ int main(int argc, const char* argv[]) {
                    .endWith("\ue0b4")
                    .priority(3)
                    .showIf(line_number > 0),
-        
+    
                   saker::Zone{ // error code of previous comand
-            
+        
                       saker::Icon{
                           " \uF071 "
                       },
-            
+        
                       std::to_string(prev_error_code)
-            
+        
                   }.bg(saker::Bg::red)
                    .priority(4)
                    .endWith("\ue0b4")
                    .showIf(prev_error_code > 0),
+    
+                  saker::Zone{ // jobs running in the background
         
+                      saker::Icon{
+                          " \uF740"
+                      },
+        
+                      std::to_string(jobs_managed)
+        
+                  }.bg(saker::Bg::red)
+                   .priority(4)
+                   .endWith("\ue0b4")
+                   .showIf(jobs_managed > 0),
+    
                   saker::Zone{ // host
-            
+        
                       saker::Icon{
                           hosticon
                       },
-            
+        
                       saker::Content{
                           hostname
                       }
@@ -80,7 +95,7 @@ int main(int argc, const char* argv[]) {
                    .priority(2)
                    .transformToFit(saker::transforming::drop_content<std::string>)
                    .endWith("\ue0b4"),
-        
+    
                   saker::Zone{ // user
             
                       saker::Icon{
@@ -94,8 +109,8 @@ int main(int argc, const char* argv[]) {
                   }.bg(userbg)
                    .transformToFit(saker::transforming::drop_content<std::string>)
                    .endWith("\ue0b4"),
-        
-        
+    
+    
                   saker::Zone{ // directory
             
                       saker::Icon{
@@ -112,8 +127,8 @@ int main(int argc, const char* argv[]) {
                    .priority(7)
                    .transformToFit(saker::transforming::drop_first_vec)
                    .endWith(git_branch.empty() ? "\ue0b0" : "\ue0c6"),
-        
-        
+    
+    
                   saker::Zone{ // git branch
             
                       saker::Icon{
@@ -130,74 +145,74 @@ int main(int argc, const char* argv[]) {
                    .endWith(git_status_counts.total > 0 ? "\ue0c6 " : "")
                    .transformToFit(saker::transforming::drop_icon<std::string>)
                    .showIf(!git_branch.empty()),
-        
-        
+    
+    
                   saker::Zone{ // git modified
             
                       saker::Content{
-                          "\uf12a"
+                          "\ufbfd "
                       }.fg(saker::FgB::blue)
             
                   }.bg(git_statuses_bg)
                    .priority(8)
                    .showIf(git_status_counts.modified > 0),
-        
-        
+    
+    
                   saker::Zone{ // git new
             
                       saker::Content{
-                          "\uf067 "
+                          "\uf916 "
                       }.fg(saker::FgB::green)
             
                   }.bg(git_statuses_bg)
                    .priority(8)
                    .showIf(git_status_counts.new_ > 0),
-        
-        
+    
+    
                   saker::Zone{ // git deleted
             
                       saker::Content{
-                          "\uf068 "
+                          "\ufb81 "
                       }.fg(saker::FgB::red)
             
                   }.bg(git_statuses_bg)
                    .priority(8)
                    .showIf(git_status_counts.deleted > 0),
-        
-        
+    
+    
                   saker::Zone{ // git renamed
             
                       saker::Content{
-                          "\uf064 "
+                          "\uf138 "
                       }.fg(saker::FgB::yellow)
             
                   }.bg(git_statuses_bg)
                    .priority(8)
                    .showIf(git_status_counts.renamed > 0),
-        
-        
+    
+    
                   saker::Zone{ // git type change
             
                       saker::Content{
-                          "\uf128 "
+                          "\uf059 "
                       }.fg(saker::FgB::yellow)
             
                   }.bg(git_statuses_bg)
                    .priority(8)
                    .showIf(git_status_counts.type_change > 0),
-        
-        
+    
+    
                   saker::Zone{ // git error
             
                       saker::Content{
-                          "\uf73a "
+                          "\uf057 "
                       }.fg(saker::FgB::gray)
             
                   }.bg(git_statuses_bg)
                    .priority(8)
                    .showIf(git_status_counts.with_errors > 0),
-        
-        
+    
+    
                   saker::Zone{ // --finish-- git info
             
                       ""
@@ -206,8 +221,8 @@ int main(int argc, const char* argv[]) {
                    .priority(9)
                    .showIf(!git_repo_dir.empty())
                    .endWith("\ue0b0"),
-        
-        
+    
+    
                   saker::Zone{ // repo subdir
             
                       saker::Content{
